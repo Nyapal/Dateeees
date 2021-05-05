3b3232b90168eab411c0aaa6db758b863cf566af
Nyapal <nyapal14@gmail.com>
2021-05-05 16:44:39 -0700

    challenge 2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',  'September', 'October', 'November', 'December']
+let mons = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug',  'Sept', 'Oct', 'Nov', 'Dec']
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let weee = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 class D {
     constructor(...args) {
         this._date = new Date(...args)
@@ -6,13 +10,42 @@ class D {
         return this._date.getFullYear()
     }
     get yr() {
-        return this._date.getYear()
+        let yr = this._date.getYear().toString()
+        return yr.slice(1, yr.length)
+    }
+    get month() {
+        let month = this._date.getMonth()
+        return months[month]
+    }
+    get mon() {
+        let mon = this._date.getMonth()
+        return mons[mon]
+    }
+    get day() {
+        let day = this._date.getDay() 
+        return week[day]
+    }
+    get dy() {
+        let day = this._date.getDay()
+        return weee[day]
+    }
+    get date() {
+        return this._date.getDate()
+    }
+    get hours() {
+        return this._date.getHours()
+    }
+    get mins() {
+        return this._date.getMinutes()
+    }
+    get secs() {
+        return this._date.getSeconds()
     }
 }
 
 const d = new D()
 
-console.log('Year', d.year )  // 2021 - Full year
+console.log( d.year )  // 2021 - Full year
 console.log( d.yr )    // 21   - Short year
 console.log( d.month ) // July - Full month
 console.log( d.mon )   // Jul  - Short month
