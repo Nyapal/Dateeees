1bd4a70b18ccb91950c1c4d42565408cd982afbf
Nyapal <nyapal14@gmail.com>
2021-05-05 16:02:21 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+class D {
+    constructor(...args) {
+        this._date = new Date(...args)
+    }
+    get year() {
+        return this._date.getFullYear()
+    }
+    get yr() {
+        return this._date.getYear()
+    }
+}
+
+const d = new D()
+
+console.log('Year', d.year )  // 2021 - Full year
+console.log( d.yr )    // 21   - Short year
+console.log( d.month ) // July - Full month
+console.log( d.mon )   // Jul  - Short month
+console.log( d.day )   // Tuesday - Full day
+console.log( d.dy )    // Tue  - Short day
+console.log( d.date )  // 27   - Date
+console.log( d.hours ) // 18   - Hour
+console.log( d.mins )  // 6    - Minutes
+console.log( d.secs )  // 5    - Seconds
